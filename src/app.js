40676f6620d996763393cf18078a676e2d9f95a7
Yuriy Marin <yuriy.marin@gmail.com>
2023-08-15 23:20:08 +0200

    feat: Добавлены шаблоны и функции управления шаблонами
    Тестовая сборка главной страницы из шаблонов.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+import {Selector} from './const';
+import {getNode, getNodeFromTemplate, placeNode} from './utils';
+
+const resultsFragment = document.createDocumentFragment();
+const resultNode = getNodeFromTemplate(Selector.RESULT_TEMPLATE)
+const recordsNode = getNodeFromTemplate(Selector.RESULT_RECORDS_TEMPLATE)
+const transactionsNode = getNodeFromTemplate(Selector.TRANSACTIONS_TEMPLATE)
+
+placeNode(resultsFragment, resultNode);
+placeNode(getNode(Selector.RESULT_RECORDS_WRAPPER, resultsFragment), recordsNode);
+placeNode(getNode(Selector.TRANSACTIONS_WRAPPER, resultsFragment), transactionsNode);
+placeNode(getNode(Selector.PAGE_CONTENT), resultsFragment);
---
+++ src/const.js
@@ -0,0 +1,8 @@
+export const Selector = {
+  PAGE_CONTENT: '.page__content',
+  RESULT_TEMPLATE: '#result',
+  RESULT_RECORDS_TEMPLATE: '#result-records',
+  TRANSACTIONS_TEMPLATE: '#transactions',
+  RESULT_RECORDS_WRAPPER: '.result',
+  TRANSACTIONS_WRAPPER: '.result__records'
+};
