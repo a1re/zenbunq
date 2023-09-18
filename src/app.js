f71c2ac733547298760a471ddcefc391c43d58c9
Yuriy Marin <yuriy.marin@gmail.com>
2023-09-19 00:33:44 +0200

    feat: Динамичная сборка таблицы транзакций

--- src/app.js
+++ src/app.js
@@ -1,24 +1,70 @@
-import {Selector} from './const';
+import {Selector, Value} from './const';
 import NodeComposer from './node-composer';
+import {mockData} from './mock-data';
 
 const composer = new NodeComposer(NodeComposer.ERROR_MODE);
-composer.composeNode({
-  wrapper: Selector.PAGE_CONTENT,
-  template: Selector.RESULT_TEMPLATE,
-  children: [{
-    wrapper: Selector.RESULT_RECORDS_WRAPPER,
-    template: Selector.RESULT_RECORDS_TEMPLATE,
-    children: [{
-      wrapper: Selector.TRANSACTIONS_WRAPPER,
-      template: Selector.TRANSACTIONS_TEMPLATE,
+
+const showTransactions = (evt) => {
+  evt.preventDefault();
+
+  const transactionRecords = [];
+  const transactionIdPrefix = 'transaction-';
+
+  for (let i = 0; i < mockData.transactions.length; i++) {
+    transactionRecords.push({
+      id: transactionIdPrefix + i,
+      wrapper: Selector.TRANSACTIONS_ROW_WRAPPER,
+      template: Selector.TRANSACTIONS_ROW_TEMPLATE,
       values: [
         {
-          wrapper: 'tr:nth-of-type(2) .transactions__counterpart',
-          innerHTML: '<b>Test</b>',
-          innerText: 'Not so test'
+          wrapper: Selector.TRANSACTION_DATE_WRAPPER,
+          innerHTML: mockData.transactions[i].date || Value.TRANSACTION_EMPTY_STRING
+        },
+        {
+          wrapper: Selector.TRANSACTION_CATEGORY_WRAPPER,
+          innerHTML: mockData.transactions[i].category || Value.TRANSACTION_EMPTY_STRING
+        },
+        {
+          wrapper: Selector.TRANSACTION_COUNTERPART_WRAPPER,
+          innerHTML: mockData.transactions[i].counterpart || Value.TRANSACTION_EMPTY_STRING
+        },
+        {
+          wrapper: Selector.TRANSACTION_PAYER_WRAPPER,
+          innerHTML: mockData.transactions[i].payer || Value.TRANSACTION_EMPTY_STRING
+        },
+        {
+          wrapper: Selector.TRANSACTION_PAYEE_WRAPPER,
+          innerHTML: mockData.transactions[i].payee || Value.TRANSACTION_EMPTY_STRING
+        },
+        {
+          wrapper: Selector.TRANSACTION_SUM_WRAPPER,
+          innerHTML: mockData.transactions[i].sum
+        },
+        {
+          wrapper: Selector.TRANSACTION_COMMENT_WRAPPER,
+          innerHTML: mockData.transactions[i].comment || ''
         }
       ]
-    }]
-  }]
-});
+    });
+  }
 
+  composer.composeNode({
+    id: 'results',
+    wrapper: Selector.PAGE_CONTENT,
+    template: Selector.RESULT_TEMPLATE,
+    children: [{
+      wrapper: Selector.RESULT_RECORDS_WRAPPER,
+      template: Selector.RESULT_RECORDS_TEMPLATE,
+      children: [{
+        id: 'transactions-table',
+        wrapper: Selector.TRANSACTIONS_WRAPPER,
+        template: Selector.TRANSACTIONS_TEMPLATE,
+        children: transactionRecords
+      }]
+    }],
+    incremental: false
+  });
+};
+
+const uploadButton = document.querySelector(Selector.UPLOAD_BUTTON);
+uploadButton.addEventListener('click', showTransactions);
---
+++ src/mock-data.js
@@ -0,0 +1,66 @@
+export const mockData = {
+  transactions: [
+    {
+      date: '22.03.2022',
+      counterpart: 'Hilversum City Apartments',
+      payer: 'Bunq | Main',
+      sum: 7.15,
+      comment: 'Jutter Speijs B.V. Hilversum, NL'
+    },
+    {
+      date: '22.03.2022',
+      category: 'Семья',
+      counterpart: 'Родители',
+      payee: 'Bunq | Main',
+      sum: 172
+    },
+    {
+      date: '22.03.2022',
+      category: 'Банковские услуги',
+      counterpart: 'Bunq',
+      payer: 'Bunq | Main',
+      sum: 5,
+      comment: 'Комиссия за входящий платеж от RaiffeisenBank Russia'
+    },
+    {
+      date: '22.03.2022',
+      category: 'Продукты',
+      counterpart: 'Albert Heijn',
+      payer: 'Bunq | Main',
+      sum: 520.39,
+      comment: 'Комиссия за входящий платеж от RaiffeisenBank Russia'
+    },
+    {
+      date: '23.03.2022',
+      category: 'Переезд',
+      counterpart: 'Booking.com',
+      payer: 'Bunq | Main',
+      sum: 59,
+      comment: 'Отель в Милане для Ани'
+    },
+    {
+      date: '23.03.2022',
+      category: 'Еда вне дома',
+      counterpart: '30ml',
+      payer: 'Bunq | Main',
+      sum: 3.8,
+      comment: 'Кофе'
+    },
+    {
+      date: '23.03.2022',
+      category: 'Продукты',
+      counterpart: 'Hilversum City Apartments',
+      payer: 'Bunq | Main',
+      sum: 2.9,
+      comment: 'Jutter Speijs B.V. Hilversum, NL'
+    },
+    {
+      date: '24.03.2022',
+      category: 'Транспорт',
+      counterpart: 'NS',
+      payer: 'Bunq | Main',
+      sum: 20,
+      comment: 'Jutter Speijs B.V. Hilversum, NL'
+    }
+  ]
+}
